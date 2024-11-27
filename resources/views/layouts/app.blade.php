9bb70cc6c623b83e6e8d2a1af55c38b4570870ca
NishakMohomed <nishakmohomed@gmail.com>
2024-11-27 17:39:32 +0530

    Added header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
             <!-- Page Content -->
             <div class="flex flex-col">
                 <!-- Header goes here -->
+                 @include('layouts.header')
                 <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
                     <div className="flex items-center">
                         <h1 className="text-lg font-semibold md:text-2xl">Advertisements</h1>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,4 @@
+<header class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+    <!-- sheet goes here -->
+    <!-- dropdown goes here -->
+</header>
